3fa3c633ec65db4cbe184c2492153ae53bd11bd8
Rafael Vicio <rafaelvicio@icloud.com>
2018-06-12 22:26:36 -0300

    Variaveis avançadas

--- fundamentos/variaveis/main.go
+++ fundamentos/variaveis/main.go
@@ -10,8 +10,21 @@ var quantidade int //quantidade = 0
 var comprou bool   //comprou = false
 var palavras rune
 
-func main() {
+//VariavelPublica e publica
+var VariavelPublica string
+var variavellPrivada string // privada
 
+//atalhos
+
+var nome, sobreNome string
+var (
+	idade      int
+	maiorIdade bool
+)
+
+func main() {
+	teste := "Valor de teste"
+	fmt.Println(teste)
 	fmt.Printf("endereco: %s\r\n", endereco)
 	fmt.Printf("quanidade: %d\r\n", quantidade)
 
