6bd9d51e18074815f09de839b8503c7d790f9b13
Rafael Vicio <rafaelvicio@icloud.com>
2018-06-12 22:06:39 -0300

    Variaveis

---
+++ fundamentos/variaveis/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+var endereco string //endereco = ""
+var telefone = "999-999"
+var quantidade int //quantidade = 0
+var comprou bool   //comprou = false
+var palavras rune
+
+func main() {
+
+	fmt.Printf("endereco: %s\r\n", endereco)
+	fmt.Printf("quanidade: %d\r\n", quantidade)
+
+}
