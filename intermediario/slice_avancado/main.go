ebbd6f2727c30af6fc574618b2fd58f904d0db6c
Rafael Vicio <rafaelvicio@icloud.com>
2018-06-18 19:58:08 -0300

    Slices

---
+++ intermediario/slice_avancado/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	cidades := make([]string, 5)
+	cidades[0] = "São Paulo"
+	cidades[1] = "Brasilia"
+	cidades[2] = "João Pessoa"
+	cidades[3] = "Rio de Janeiro"
+	cidades[4] = "Sao Luiz"
+
+	cidadesVizinhas := cidades[1:3]
+	fmt.Println(cidadesVizinhas)
+
+	primeiras := cidades[:2]
+	fmt.Println(primeiras)
+
+	ultimas := cidades[3:]
+	fmt.Println(ultimas)
+}
